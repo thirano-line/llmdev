d0adb5f55f435e3a1b32e7f157f4d2af8f1d1888
thirano-line <foo@lycorp.co.jp>
2025-08-08 11:07:37 +0900

    テストコードの課題完了

---
+++ 06_test/test_authenticator.py
@@ -0,0 +1,26 @@
+import authenticator
+import pytest
+
+def test_register():
+    obj = authenticator.Authenticator()
+    obj.register("user", "pass")
+    assert obj.users["user"] == "pass"
+
+
+def test_register_duplicate():
+    with pytest.raises(ValueError, match="エラー: ユーザーは既に存在します。"):
+        obj = authenticator.Authenticator()
+        obj.register("user", "pass")
+        obj.register("user", "pass")
+
+def test_login():
+    obj = authenticator.Authenticator()
+    obj.register("user", "pass")
+    result = obj.login("user", "pass")
+    assert result == "ログイン成功"
+
+def test_login_fail():
+    with pytest.raises(ValueError, match="エラー: ユーザー名またはパスワードが正しくありません。"):
+        obj = authenticator.Authenticator()
+        obj.register("user", "pass")
+        result = obj.login("user", "wrong")
